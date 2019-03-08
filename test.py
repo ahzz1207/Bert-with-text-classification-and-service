0077ef426b851f2880f10c5ea0fc2ccbf93be79a
ahzz1207 <huangzhuangze@qq.com>
2019-03-08 19:42:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import test_seving as service
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 session, FLAGS,processor, tokenizer, label_real, predict_file = service.getSess()
 
-sentence = "make a test"
-
-result = service.predict(session, FLAGS, processor, tokenizer, label_real, predict_file, sentence)
+sentence = ["Add a record (name and data) to the index.", "Tells JavaScript to open windows automatically.","Just a test."]
+for s in sentence:
+    result = service.predict(session, FLAGS, processor, tokenizer, label_real, predict_file, s)
+    print(result)
